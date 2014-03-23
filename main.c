fc7050e877a7adfdc7dc980809742565eff6ff6b
halit <info@halitalptekin.com>
2014-03-23 17:04:15 +0200

    Add fibonacci example

--- main.c
+++ main.c
@@ -74,8 +74,38 @@ void third_example(virtual_machine* my_vm){
     execute(my_vm);
 }
 
+void fib_example(virtual_machine* my_vm){
+    my_vm->registers[1] = 6; // n + 1 = 6, n = 5
+    my_vm->registers[2] = 1; // fib(0) = 1
+    my_vm->registers[3] = 1; // fib(1) = 1
+    my_vm->registers[4] = 0; // fib(n)
+
+    Registers A, B, D;
+
+    my_vm->car[0x0] = LZ | 0x5; // if z == 1 goto: 6
+    A = R2;
+    D = make_register(R4, 0);
+    my_vm->car[0x1] = A | D | TSF; // R4 <- R2
+
+    A = R3;
+    B = make_register(R2, 1);
+    D = make_register(R2, 0);
+    my_vm->car[0x2] = A | B | D | ADD; // R2 <- R2 + R3
+
+    A = R4;
+    D = make_register(R3, 0);
+    my_vm->car[0x3] = A | D | TSF; // R3 <- R4
+
+    A = R1;
+    D = make_register(R1, 0);
+    my_vm->car[0x4] = A | D | DEC | LAD | 0x0; // R1 <- R1 - 1
+
+    my_vm->car[0x5] = HLT;
+
+    execute(my_vm);
+}
 int main(){
     virtual_machine* my_vm = machine_init(8, 32);
-    third_example(my_vm);
+    fib_example(my_vm);
 
 }
